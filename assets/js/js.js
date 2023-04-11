56fb814aaa7b2a45c3414ad0d94bcd74a6386376
simo994q <simonkn1@hotmail.com>
2023-04-11 09:24:58 +0200

    api confirm

--- assets/js/js.js
+++ assets/js/js.js
@@ -6,40 +6,44 @@ const api = "https://api.open-meteo.com/v1/forecast?latitude=57.05&longitude=9.9
 buildStateOne()
 
 function buildStateOne() {
-    myApp.innerHTML = ""
+  myApp.innerHTML = ""
 
-    myApp.classList.add('appStateOne')
+  myApp.classList.add('appStateOne')
 
-    let loadingTitle = document.createElement('h1')
-    loadingTitle.innerHTML = "wethr"
-    loadingTitle.classList.add('s1LoadingTitle')
+  let loadingTitle = document.createElement('h1')
+  loadingTitle.innerHTML = "wethr"
+  loadingTitle.classList.add('s1LoadingTitle')
 
-    let loadingAnimation = document.createElement('img')
-    loadingAnimation.src = "assets/images/svg/loading-svgrepo-com (2).svg"
-    loadingAnimation.classList.add('s1LoadingAnimation')
+  let loadingAnimation = document.createElement('img')
+  loadingAnimation.src = "assets/images/svg/loading-svgrepo-com (2).svg"
+  loadingAnimation.classList.add('s1LoadingAnimation')
 
-    let loadingText = document.createElement('h2')
-    loadingText.innerHTML = "LOADING"
-    loadingText.classList.add('s1LoadingText')
-
-
-    myApp.appendChild(loadingTitle)
-    myApp.appendChild(loadingAnimation)
-    myApp.appendChild(loadingText)
+  let loadingText = document.createElement('h2')
+  loadingText.innerHTML = "LOADING"
+  loadingText.classList.add('s1LoadingText')
 
 
-    let apiTest = fetch(api)
-        .then((apiResponse) => {
-            return apiResponse.json()
-        })
-        .then((jsonResponse) => {
-            weatherData = jsonResponse
-            buildStateTwo(weatherData)
-            console.log(apiTest);
-        })
-        .catch((error) => {
-            console.error(error)
-        })
+  myApp.appendChild(loadingTitle)
+  myApp.appendChild(loadingAnimation)
+  myApp.appendChild(loadingText)
+
+
+  let apiTest = fetch(api)
+    .then((apiResponse) => {
+      if (apiResponse.ok == true) {
+        return apiResponse.json()
+      } else if (apiResponse.ok == false) {
+        loadingText.innerHTML = "NO DATA"
+      }
+    })
+    .then((jsonResponse) => {
+      weatherData = jsonResponse
+        buildStateTwo(weatherData)
+      console.log(apiTest);
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 
 };
 
@@ -50,110 +54,110 @@ function buildStateOne() {
 
 
 let myFakeData = {
-    location: "Aalborg",
-    status: "assets/images/svg/Icon weather-day-sunny-5.svg",
-    temp: "15",
-    time: "09:00",
-    date: "Mandag 23 januar",
-    wind: "2.6",
-    rain: "0.3"
+  location: "Aalborg",
+  status: "assets/images/svg/Icon weather-day-sunny-5.svg",
+  temp: "15",
+  time: "09:00",
+  date: "Mandag 23 januar",
+  wind: "2.6",
+  rain: "0.3"
 }
 
 
 function buildStateTwo(data) {
-    myApp.innerHTML = ""
-    document.title = "App"
+  myApp.innerHTML = ""
+  document.title = "App"
 
-    let precipitation = data.hourly.precipitation
-    let temperature = data.hourly.temperature_2m
-    let time = data.hourly.time
-    let windDirection = data.hourly.winddirection_10m
-    let windSpeed = data.hourly.windspeed_10m
+  let precipitation = data.hourly.precipitation
+  let temperature = data.hourly.temperature_2m
+  let time = data.hourly.time
+  let windDirection = data.hourly.winddirection_10m
+  let windSpeed = data.hourly.windspeed_10m
 
-    myApp.classList.replace('appStateOne', 'appStateTwo')
+  myApp.classList.replace('appStateOne', 'appStateTwo')
 
-    let fullLocation = document.createElement('div')
-    fullLocation.classList.add('s2FullLocation')
+  let fullLocation = document.createElement('div')
+  fullLocation.classList.add('s2FullLocation')
 
-    let dayLocation = document.createElement('h1')
-    dayLocation.innerHTML = data.timezone
-    dayLocation.classList.add('s2DayLocation')
-    
-    let locationSvg = document.createElement('img')
-    locationSvg.classList.add('s2LocationSvg')
-    locationSvg.src = "assets/images/svg/location_on_FILL-1.svg"
+  let dayLocation = document.createElement('h1')
+  dayLocation.innerHTML = data.timezone
+  dayLocation.classList.add('s2DayLocation')
 
-    let dayStatus = document.createElement('img') //TODO Switch-funktion til status
-    dayStatus.src = "assets/images/svg/Icon weather-day-sunny-5.svg"
-    dayStatus.classList.add('s2DayStatus')
+  let locationSvg = document.createElement('img')
+  locationSvg.classList.add('s2LocationSvg')
+  locationSvg.src = "assets/images/svg/location_on_FILL-1.svg"
 
-    let dayTemp = document.createElement('h2')
-    dayTemp.innerHTML = `${temperature[0]}°`
-    dayTemp.classList.add('s2DayTemp')
+  let dayStatus = document.createElement('img') //TODO Switch-funktion til status
+  dayStatus.src = "assets/images/svg/Icon weather-day-sunny-5.svg"
+  dayStatus.classList.add('s2DayStatus')
 
-    let fullDate = document.createElement('div')
-    fullDate.classList.add('s2FullDate')
+  let dayTemp = document.createElement('h2')
+  dayTemp.innerHTML = `${temperature[0]}°`
+  dayTemp.classList.add('s2DayTemp')
 
-    let dayTime = document.createElement('h3')
-    dayTime.innerHTML = data.hourly.time[0].slice(-5)
-    dayTime.classList.add('s2DayTime')
+  let fullDate = document.createElement('div')
+  fullDate.classList.add('s2FullDate')
 
-    let dayDate = document.createElement('p')
-    dayDate.innerHTML = data.hourly.time[0].slice(0, 10)
-    dayDate.classList.add('s2DayDate')
+  let dayTime = document.createElement('h3')
+  dayTime.innerHTML = data.hourly.time[0].slice(-5)
+  dayTime.classList.add('s2DayTime')
 
-
-    let WindRainDiv = document.createElement('div')
-    WindRainDiv.classList.add('s2WindRainDiv')
-
-    let fullWind = document.createElement('div')
-    fullWind.classList.add('s2FullWindRain')
-    let dayWind = document.createElement('p')
-    dayWind.innerHTML = windSpeed[0]
-    dayWind.classList.add('s2DayWindRainP')
-    let windMetric = document.createElement('p')
-    windMetric.innerHTML = data.hourly_units.windspeed_10m
-    windMetric.classList.add('s2WindRainMetric')
-
-    let fullRain = document.createElement('div')
-    fullRain.classList.add('s2FullWindRain')
-    let dayRain = document.createElement('p')
-    dayRain.innerHTML = precipitation[0]
-    dayRain.classList.add('s2DayWindRainP')
-    let rainMetric = document.createElement('p')
-    rainMetric.innerHTML = data.hourly_units.precipitation
-    rainMetric.classList.add('s2WindRainMetric')
-
-    myApp.appendChild(fullLocation)
-    fullLocation.appendChild(dayLocation)
-    fullLocation.appendChild(locationSvg)
-    myApp.appendChild(dayTemp)
-    myApp.appendChild(dayStatus)
-    myApp.appendChild(fullDate)
-    fullDate.appendChild(dayTime)
-    fullDate.appendChild(dayDate)
-
-    myApp.appendChild(WindRainDiv)
-    WindRainDiv.appendChild(fullWind)
-    fullWind.appendChild(dayWind)
-    fullWind.appendChild(windMetric)
-    WindRainDiv.appendChild(fullRain)
-    fullRain.appendChild(dayRain)
-    fullRain.appendChild(rainMetric)
-
-    
-        console.log(data);
-    //TODO Returnere et object med flere objects og arrays med data
-
-    //TODO "let dataindex = 0" dataindex skal ændre sig når man swiper
+  let dayDate = document.createElement('p')
+  dayDate.innerHTML = data.hourly.time[0].slice(0, 10)
+  dayDate.classList.add('s2DayDate')
 
 
-    //* Arrays
-    buildNavigationBar()
+  let WindRainDiv = document.createElement('div')
+  WindRainDiv.classList.add('s2WindRainDiv')
+
+  let fullWind = document.createElement('div')
+  fullWind.classList.add('s2FullWindRain')
+  let dayWind = document.createElement('p')
+  dayWind.innerHTML = windSpeed[0]
+  dayWind.classList.add('s2DayWindRainP')
+  let windMetric = document.createElement('p')
+  windMetric.innerHTML = data.hourly_units.windspeed_10m
+  windMetric.classList.add('s2WindRainMetric')
+
+  let fullRain = document.createElement('div')
+  fullRain.classList.add('s2FullWindRain')
+  let dayRain = document.createElement('p')
+  dayRain.innerHTML = precipitation[0]
+  dayRain.classList.add('s2DayWindRainP')
+  let rainMetric = document.createElement('p')
+  rainMetric.innerHTML = data.hourly_units.precipitation
+  rainMetric.classList.add('s2WindRainMetric')
+
+  myApp.appendChild(fullLocation)
+  fullLocation.appendChild(dayLocation)
+  fullLocation.appendChild(locationSvg)
+  myApp.appendChild(dayTemp)
+  myApp.appendChild(dayStatus)
+  myApp.appendChild(fullDate)
+  fullDate.appendChild(dayTime)
+  fullDate.appendChild(dayDate)
+
+  myApp.appendChild(WindRainDiv)
+  WindRainDiv.appendChild(fullWind)
+  fullWind.appendChild(dayWind)
+  fullWind.appendChild(windMetric)
+  WindRainDiv.appendChild(fullRain)
+  fullRain.appendChild(dayRain)
+  fullRain.appendChild(rainMetric)
+
+
+  console.log(data);
+  //TODO Returnere et object med flere objects og arrays med data
+
+  //TODO "let dataindex = 0" dataindex skal ændre sig når man swiper
+
+
+  //* Arrays
+  // buildNavigationBar()
 };
-function buildNavigationBar () {
-  let fullNavBar = 
-};
+// function buildNavigationBar () {
+//   let fullNavBar = 
+// };
 
 
 
@@ -169,5 +173,5 @@ function getLocation() {
 
 function showPosition(position) {
   console.log("Latitude: " + position.coords.latitude +
-  " Longitude: " + position.coords.longitude)
+    " Longitude: " + position.coords.longitude)
 }
